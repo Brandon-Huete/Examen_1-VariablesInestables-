4c2915407b30ade33d46be3ad7588af7eaef3795
BrandonHuete <brhuetero@est.utn.ac.cr>
2025-07-04 16:15:35 -0600

    Se crea la lista de Deportista
    Se crea la lista de Deportistas con este se crea el vector

---
+++ src/Personas/ListaDeDeportista.java
@@ -0,0 +1,23 @@
+1/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Personas;
+
+import Personas.Deportista;
+
+import Listas.Lista;
+
+/**
+ *
+ * @author Student
+ */
+public class ListaDeDeportista implements Lista <Deportista> {
+    ListaDeDeportista[]Lista1 = new Lista1[200];
+    
+            
+            
+    
+    
+    
+}
